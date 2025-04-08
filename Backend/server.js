e681b3d3f50f1d51127bfbb2f03fa274f4fb094a
shivendra0712 <patelshivendra817@gmail.com>
2025-04-08 23:25:48 +0530

    updated port number

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const connect = require('./src/db/db')
 
 connect();
 
-app.listen(3000 , ()=>{
-    console.log(`server is started on http://localhost:${3000}`)
+const port = process.env.PORT || 5000;
+
+app.listen(port , ()=>{
+    console.log(`server is started`)
 })
