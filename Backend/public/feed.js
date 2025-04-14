6315720ddca49889e9b949e9d30953a0bb4ddb61
shivendra0712 <patelshivendra817@gmail.com>
2025-04-14 15:55:50 +0530

    added feed.js file

---
+++ Backend/public/feed.js
@@ -0,0 +1,5 @@
+const userprofile = document.querySelector('.userprofile');
+const profile = document.querySelector('#profile');
+console.log(userprofile)
+console.log(profile)
+console.log('profile');
